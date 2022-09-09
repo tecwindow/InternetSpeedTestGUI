65c85103a5086aebd639b318ac33d2dd4f8020d6
tecwindow <109696849+tecwindow@users.noreply.github.com>
2022-09-09 17:40:15 +0200

    Add files via upload

---
+++ InternetSpeedTest.py
@@ -0,0 +1,115 @@
+﻿#python3.9
+# import project libraries.
+import wx
+import speedtest
+import requests
+import webbrowser
+from ViewInfo import *
+
+# Create app with wx.
+app= wx.App()
+
+# Create main window with wx.
+class SpeedTest(wx.Frame):
+	def __init__(self):
+		wx.Frame.__init__(self, None, title = 'Internet Speed Test', size=(550, 350))
+
+		#make window in center.
+		self.Center()
+
+		#make window Minimum size.
+		self.Maximize(False)
+		self.EnableMaximizeButton(False)
+
+
+		# Creating panel
+		Panel = wx.Panel(self)
+
+		# Creating Buttons
+		self.UploadSpeed = wx.Button(Panel, -1, "UploadSpeed", pos=(350,40), size=(100,30))
+		self.DownloadSpeed = wx.Button(Panel, -1, "DownloadSpeed", pos=(100,40), size=(100,30))
+		self.BothDownloadAndUpload = wx.Button(Panel, -1, "BothDownloadAndUpload", pos=(180,90), size=(160,30))
+		self.PingInfo = wx.Button(Panel, -1, "PingInfo", pos=(350,120), size=(100,30))
+		self.IpInfo = wx.Button(Panel, -1, "IpInfo", pos=(70,120), size=(100,30))
+		self.Help = wx.Button(Panel, -1, "Help", pos=(20,250), size=(60,30))
+		self.Close = wx.Button(Panel, -1, "Close", pos=(450,250), size=(60,30))
+
+
+		# Show Main window
+		self.Show()
+
+		# events for buttons
+		self.IpInfo.Bind(wx.EVT_BUTTON, self.OnIpInfo)
+		self.Help.Bind(wx.EVT_BUTTON, self.OnHelpMenu)
+		self.Close.Bind(wx.EVT_BUTTON, self.OnCloseProgram)
+		self.UploadSpeed.Bind(wx.EVT_BUTTON, self.OnUploadSpeed)
+		self.DownloadSpeed.Bind(wx.EVT_BUTTON, self.OnDownloadSpeed)
+		self.BothDownloadAndUpload.Bind(wx.EVT_BUTTON, self.OnBothDownloadAndUpload)
+		self.PingInfo.Bind(wx.EVT_BUTTON, self.OnPingInfo)
+
+	# Functions
+	#Get Upload Speed Function
+	def OnUploadSpeed(self, event):
+		ViewInfoDialog(self, "upload")
+
+	#Get Download Speed Function
+	def OnDownloadSpeed(self, eve):
+		ViewInfoDialog(self, "download")
+
+	#Get Upload And Download Speed Function
+	def OnBothDownloadAndUpload(self, event):
+		ViewInfoDialog(self, "download and upload")
+
+	#Get Ping Info  Speed Function
+	def OnPingInfo(self, event):
+		ViewInfoDialog(self, "ping")
+
+	# IP Info Function
+	def OnIpInfo(self, event):
+		IPMenu = wx.Menu()
+		GetMyIPItem = IPMenu.Append(-1, "Get My IP")
+		GetMyIPInfoItem = IPMenu.Append(-1, "Get My IP Info")
+		GetExternalIPInfoItem = IPMenu.Append(-1, "Get external IP Info")
+		self.Bind(wx.EVT_MENU, self.OnGetMyIP, GetMyIPItem)
+		self.Bind(wx.EVT_MENU, self.OnGetMyIPInfo, GetMyIPInfoItem)
+		self.Bind(wx.EVT_MENU, self.OnGetExternalIPInfo, GetExternalIPInfoItem)
+		self.PopupMenu(IPMenu)
+
+	# IPInfo Menu Item Functions
+	def OnGetMyIP(self, event):
+		ViewInfoDialog(self, "get ip")
+
+	def OnGetMyIPInfo(self, event):
+		ViewInfoDialog(self, "ip info")
+
+	def OnGetExternalIPInfo(self, event):
+		ip = wx.GetTextFromUser("Input the external IP", "external IP")
+		if ip:
+			ViewInfoDialog(self, "get external IP Info", ip)
+
+	# HelpMenu Functions
+	def OnHelpMenu(self, event):
+		HelpMenu = wx.Menu()
+		HelpItem = HelpMenu.Append(-1, "Help")
+		WebSiteItem = HelpMenu.Append(-1, "Visit WebSite")
+		AboutItem = HelpMenu.Append(-1, "about")
+		self.Bind(wx.EVT_MENU, self.OnHelp, HelpItem)
+		self.Bind(wx.EVT_MENU, self.OnWebSite, WebSiteItem)
+		self.Bind(wx.EVT_MENU, self.OnAbout, AboutItem)
+		self.PopupMenu(HelpMenu)
+
+	# Help Menu Item Functions
+	def OnHelp(self, event):
+		pass
+	def OnWebSite(self, event):
+		webbrowser.open_new('https://t.me/A2zTecChannel')
+	def OnAbout(self, event):
+		wx.MessageBox('With this tool, you can check your internet speed. Where you can know the download speed, upload speed and ping rate. You can also find out your IP address, and find out all the information about any IP that you type into the tool.', 'About Tool')
+
+
+	# CloseProgram
+	def OnCloseProgram(self, event):
+		wx.Exit()
+
+SpeedTest()
+app.MainLoop()    
---
+++ ViewInfo.py
@@ -0,0 +1,187 @@
+﻿# import project libraries.
+import wx
+import speedtest
+import requests
+import threading
+import ctypes
+import pyperclip
+
+
+try:
+	speed = speedtest.Speedtest()
+except:
+	pass
+
+
+# Create dialog window To ViewInfo
+class ViewInfoDialog(wx.Dialog):
+	def __init__(self, parent, service, ip=None):
+		wx.Dialog.__init__(self, parent, title='View Info', size=(300, 400))
+		#make window in center.
+		self.Center()
+		self.service = service
+		self.ip = ip
+
+
+
+		# Creating panel
+		Panel = wx.Panel(self)
+
+		# Create RichEdit to View Info
+		self.title = wx.StaticText(Panel, -1, "Please wait.", pos=(10,10), size=(380,30))
+		self.ViewInfo = wx.TextCtrl(Panel, -1, pos=(10,40), size=(270,250), style=wx.TE_RICH2+wx.TE_MULTILINE+wx.TE_READONLY)
+		self.ThreadLoadInfo = thread(target=self.LoadInfo, daemon=True)
+		self.ThreadLoadInfo.start()
+
+
+		# Creating Buttons
+		self.CopyInfo = wx.Button(Panel, -1, "Copy Info", pos=(10,310), size=(60,30))
+		self.Close = wx.Button(Panel, wx.ID_CANCEL, "Close", pos=(200,310), size=(60,30))
+
+
+		# events for buttons
+		self.Close.Bind(wx.EVT_BUTTON, self.OnClose)
+		self.CopyInfo.Bind(wx.EVT_BUTTON, self.OnCopyInfo)
+
+		# Show Main window
+		self.Show()
+
+	#creating function to load the information
+	def LoadInfo(self):
+		#set the best server in case test download or upload or ping.
+		if self.service in ("download", "upload", "download and upload", "ping"):
+			speed.get_best_server()
+
+		#getting speed download and display it.
+		if self.service == "download":
+			DownloadSpeed = round(speed.download()/1000000, 3)
+			self.ViewInfo.Value = """Your speed test has completed.
+your download speed is {}Mbps.
+""".format(DownloadSpeed)
+
+		#getting speed upload and display it.
+		elif self.service == "upload":
+			UploadSpeed = round(speed.upload()/1000000, 3)
+			self.ViewInfo.Value = """Your speed test has completed.
+your upload speed is {}Mbps.
+""".format(UploadSpeed)
+
+		#geting speed download and upload and display it.
+		elif self.service == "download and upload":
+			DownloadSpeed = round(speed.download()/1000000, 3)
+			UploadSpeed = round(speed.upload()/1000000, 3)
+			self.ViewInfo.Value = """Your speed test has completed.
+your download speed is {}Mbps,
+youre upload speed is {}Mbps.
+""".format(DownloadSpeed, UploadSpeed)
+
+	#getting pingand display it.
+		elif self.service == "ping":
+			ping = round(speed.results.ping, 1)
+			self.ViewInfo.Value = """Your speed test has completed.
+your ping is {}ms
+""".format(ping)
+
+		#getting current IP and display it.
+		elif self.service == "get ip":
+			self.ViewInfo.Value = "Your IP is {}.".format(get_ip())
+
+		#getting information of current IP and display it.
+		elif self.service == "ip info":
+			ip = get_ip()
+			try:
+				info = get_ip_information(ip)
+			except:
+				info = get_location(ip)
+			self.ViewInfo.Value = """ information has loaded.
+IP: {}
+country: {}.
+country code: {}
+city: {}
+region: {}
+""".format(info["ip"], info["country"], info["countryCode"], info["city"], info["region"])
+
+		#getting information of external IP and display it.
+		elif self.service == "get external IP Info":
+			ip = self.ip
+			try:
+				info = get_ip_information(ip)
+			except:
+				info = get_location(ip)
+			self.ViewInfo.Value = """ information has loaded.
+IP: {}
+country: {}.
+country code: {}
+city: {}
+region: {}
+""".format(info["ip"], info["country"], info["countryCode"], info["city"], info["region"])
+
+		#After loading, the title will be set to information.
+		self.title.SetLabel("information")
+
+	#copy the information.
+	def OnCopyInfo(self, ev):
+		pyperclip.copy(self.ViewInfo.Value)
+
+	#close the dialog
+	def OnClose(self, ev):
+		self.ThreadLoadInfo.stop()
+		self.Destroy()
+
+#end of class
+
+
+#creating custom thread to able me to stop it.
+class thread(threading.Thread):
+
+	def get_id(self):
+		# returns id of the respective thread
+		if hasattr(self, '_thread_id'):
+			return self._thread_id
+		for id, thread in threading._active.items():
+			if thread is self:
+				return id
+  
+	def stop(self):
+		self._is_stopped = True
+		self.thread_id = self.get_id()
+		self.res = ctypes.pythonapi.PyThreadState_SetAsyncExc(self.thread_id, ctypes.py_object(SystemExit))
+		if self.res > 1:
+			ctypes.pythonapi.PyThreadState_SetAsyncExc(self.thread_id, 0)
+
+	def is_stopped(self):
+		return self._is_stopped
+
+	#end of class.
+
+
+#creating function to get current IP.
+def get_ip():
+	response = requests.get('https://api64.ipify.org?format=json').json()
+	return response["ip"]
+
+#creating function to get IP information.
+def get_location(ip):
+	ip_address = ip
+	response = requests.get(f"https://ipapi.co/{ip_address}/json/").json()
+	location_data = {
+	"ip": ip_address,
+	"country": response.get("country_name"),
+	"countryCode": response.get("country_code"),
+	"city": response.get("city"),
+	"region": response.get("region")
+	}
+	return location_data
+
+#creating Additional function to get IP information.
+def get_ip_information(ip):
+	response = requests.get(f'http://ip-api.com/json/{ip}').json()
+	location_data = {
+	"ip": response.get("query"),
+	"country": response.get("country"),
+	"countryCode": response.get("countryCode"),
+	"city": response.get("city"),
+	"region": response.get("regionName")
+	}
+	return location_data
+
